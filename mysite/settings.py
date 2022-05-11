993c8b14746cd640d18d979ceb22f1162bce2b1a
Vinicius Cardoso <vinicius.mct17@gmail.com>
2022-05-11 12:17:59 -0300

    atualizacoes

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!_z7dg$)b1$i=ojqr4jgxq)ubt_ihht2lzghz4uqb4%)xh_bei
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'viniciusc.pythonanywhere.com']
 
 
 # Application definition
